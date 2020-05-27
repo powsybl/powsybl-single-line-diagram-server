55d4b87b56c00de8fe682613b7e288f35ebfcad7
Etienne <etiennehomer@gmai.com>
2020-05-27 15:16:28 +0200

    Remove checkstyle.xml + Suppress warning on Springboot class + change maven.yml + add missing build plugins
    Signed-off-by: Etienne <etiennehomer@gmai.com>

--- src/main/java/com/powsybl/sld/server/SingleLineDiagramApplication.java
+++ src/main/java/com/powsybl/sld/server/SingleLineDiagramApplication.java
@@ -16,7 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SingleLineDiagramApplication {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         SpringApplication.run(SingleLineDiagramApplication.class, args);
     }
 }
