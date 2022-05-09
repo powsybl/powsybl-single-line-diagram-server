16a8964cf2085e80a54746c1645ce03bc370be02
Etienne Homer <etiennehomer@gmail.com>
2022-05-09 15:22:19 +0200

    clean
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/com/powsybl/sld/server/SingleLineDiagramController.java
+++ src/main/java/com/powsybl/sld/server/SingleLineDiagramController.java
@@ -194,12 +194,8 @@ public class SingleLineDiagramController {
             @Parameter(description = "Voltage levels ids") @RequestParam(name = "voltageLevelsIds", required = false) List<String> voltageLevelsIds,
             @Parameter(description = "Variant Id") @RequestParam(name = "variantId", required = false) String variantId,
             @Parameter(description = "depth") @RequestParam(name = "depth", required = false) int depth) {
-        LOGGER.debug("getNetworkAreaDiagramSvg request received with parameter networkUuid = {}, voltageLevelsIds = {}, depth = {}", networkUuid, sanitizeParameter(voltageLevelsIds.toString()), depth);
+        LOGGER.debug("getNetworkAreaDiagramSvg request received with parameter networkUuid = {}, voltageLevelsIds = {}, depth = {}", networkUuid, voltageLevelsIds, depth);
 
         return networkAeraDiagramService.generateNetworkAreaDiagramSvg(networkUuid, variantId, voltageLevelsIds, depth);
-    }
-
-    public static String sanitizeParameter(String param) {
-        return param.replaceAll("[\n\r\t]", "_");
     }
 }
