7a8ed1d9a16b42fecdd59782a08538400639e0d4
Etienne Homer <etiennehomer@gmail.com>
2022-05-09 14:47:20 +0200

    rename cleanParameter method
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/com/powsybl/sld/server/SingleLineDiagramController.java
+++ src/main/java/com/powsybl/sld/server/SingleLineDiagramController.java
@@ -194,12 +194,12 @@ public class SingleLineDiagramController {
             @Parameter(description = "Voltage levels ids") @RequestParam(name = "voltageLevelsIds", required = false) List<String> voltageLevelsIds,
             @Parameter(description = "Variant Id") @RequestParam(name = "variantId", required = false) String variantId,
             @Parameter(description = "depth") @RequestParam(name = "depth", required = false) int depth) {
-        LOGGER.debug("getNetworkAreaDiagramSvg request received with parameter networkUuid = {}, voltageLevelsIds = {}, depth = {}", cleanParameter(networkUuid.toString()), cleanParameter(voltageLevelsIds.toString()), cleanParameter(String.valueOf(depth)));
+        LOGGER.debug("getNetworkAreaDiagramSvg request received with parameter networkUuid = {}, voltageLevelsIds = {}, depth = {}", sanitizeParameter(networkUuid.toString()), sanitizeParameter(voltageLevelsIds.toString()), sanitizeParameter(String.valueOf(depth)));
 
         return networkAeraDiagramService.generateNetworkAreaDiagramSvg(networkUuid, variantId, voltageLevelsIds, depth);
     }
 
-    public static String cleanParameter(String param) {
+    public static String sanitizeParameter(String param) {
         return param.replaceAll("[\n\r\t]", "_");
     }
 }
