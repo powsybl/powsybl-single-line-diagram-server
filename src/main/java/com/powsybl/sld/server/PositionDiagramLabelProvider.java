58cebc540e1a82eff39a26021068b2897936a265
bendaamerahm <ahmed.bendaamer@rte-france.com>
2022-11-24 15:32:42 +0100

    add Copyright && author to PositionDiagramLabelProvider file
    Signed-off-by: bendaamerahm <ahmed.bendaamer@rte-france.com>

--- src/main/java/com/powsybl/sld/server/PositionDiagramLabelProvider.java
+++ src/main/java/com/powsybl/sld/server/PositionDiagramLabelProvider.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.sld.server;
 
 import com.powsybl.commons.PowsyblException;
@@ -24,6 +30,9 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * @author Ben Daamer ahmed<ahmed.bendaamer at rte-france.com>
+ */
 public class PositionDiagramLabelProvider extends DefaultDiagramLabelProvider {
 
     private final Network network;
