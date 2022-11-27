b08f120aac059cf793bbfc2205d3930c09ff3780
bendaamerahm <ahmed.bendaamer@rte-france.com>
2022-11-27 23:13:12 +0100

    add Copyright && Author
    Signed-off-by: bendaamerahm <ahmed.bendaamer@rte-france.com>

--- src/main/java/com/powsybl/sld/server/utils/DisplayMode.java
+++ src/main/java/com/powsybl/sld/server/utils/DisplayMode.java
@@ -1,16 +1,15 @@
+/**
+ * Copyright (c) 2022, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.sld.server.utils;
 
+/**
+ * @author Ben Daamer ahmed<ahmed.bendaamer at rte-france.com>
+ */
 public enum DisplayMode {
-    FEEDER_POSITION("FEEDER_POSITION"),
-    DEFAULT("DEFAULT");
-    private final String value;
-
-    DisplayMode(String value) {
-        this.value = value;
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
+    FEEDER_POSITION,
+    DEFAULT;
 }
