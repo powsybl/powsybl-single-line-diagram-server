3e8a39fb0930dbfc3a8f39775cd05f6482f75dbf
maissa SOUISSI <maissa.souissi@rte-france.com>
2024-08-16 12:04:03 +0200

    rename new params to initialize NAD with GeoData
    Signed-off-by: maissa SOUISSI <maissa.souissi@rte-france.com>

--- src/main/java/com/powsybl/sld/server/SingleLineDiagramController.java
+++ src/main/java/com/powsybl/sld/server/SingleLineDiagramController.java
@@ -279,17 +279,17 @@ public class SingleLineDiagramController {
             @Parameter(description = "Voltage levels ids") @RequestParam(name = "voltageLevelsIds", required = false) List<String> voltageLevelsIds,
             @Parameter(description = "Variant Id") @RequestParam(name = "variantId", required = false) String variantId,
             @Parameter(description = "depth") @RequestParam(name = "depth", required = false) int depth,
-            @Parameter(description = "Initialize NAD with Geographical Data") @RequestParam(name = "initGeoData", defaultValue = "true") boolean initGeoData) throws InterruptedException, ExecutionException {
+            @Parameter(description = "Initialize NAD with Geographical Data") @RequestParam(name = "withGeoData", defaultValue = "true") boolean withGeoData) throws InterruptedException, ExecutionException {
         CompletableFuture<String> futureNAD = CompletableFuture.supplyAsync(() -> {
             try {
                 LOGGER.debug("getNetworkAreaDiagramSvg request received with parameter networkUuid = {}, voltageLevelsIds = {}, depth = {}{}",
                         networkUuid,
                         sanitizeParam(voltageLevelsIds.toString()),
                         depth,
-                        initGeoData ? ", based on geographical data." : ""
+                        withGeoData ? ", based on geographical data." : ""
                 );
 
-                SvgAndMetadata svgAndMetadata = networkAreaDiagramService.generateNetworkAreaDiagramSvg(networkUuid, variantId, voltageLevelsIds, depth, initGeoData);
+                SvgAndMetadata svgAndMetadata = networkAreaDiagramService.generateNetworkAreaDiagramSvg(networkUuid, variantId, voltageLevelsIds, depth, withGeoData);
                 String svg = svgAndMetadata.getSvg();
                 Object additionalMetadata = svgAndMetadata.getAdditionalMetadata();
                 return OBJECT_MAPPER.writeValueAsString(
