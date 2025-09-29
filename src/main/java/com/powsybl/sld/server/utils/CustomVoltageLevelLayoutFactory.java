7afeb04c76c224bc5230916fb73971b2c5b1c413
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2025-09-29 16:03:20 +0200

    fix: author and copyright missing
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/main/java/com/powsybl/sld/server/utils/CustomVoltageLevelLayoutFactory.java
+++ src/main/java/com/powsybl/sld/server/utils/CustomVoltageLevelLayoutFactory.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.sld.server.utils;
 
 import com.powsybl.iidm.network.Connectable;
@@ -12,6 +18,9 @@ import com.powsybl.sld.model.graphs.VoltageLevelGraph;
 
 import java.util.Objects;
 
+/**
+ * @author Kevin Le Saulnier <kevin.le-saulnier at rte-france.com>
+ */
 //TODO: to remove once SmartVoltageLevelLayoutFactory allow us to pass PositionVoltageLevelLayoutFactoryParameters to VoltageLevelLayoutFactory
 public class CustomVoltageLevelLayoutFactory implements VoltageLevelLayoutFactory {
     private final Network network;
--- src/main/java/com/powsybl/sld/server/utils/CustomVoltageLevelLayoutFactoryCreator.java
+++ src/main/java/com/powsybl/sld/server/utils/CustomVoltageLevelLayoutFactoryCreator.java
@@ -1,7 +1,16 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.sld.server.utils;
 
 import com.powsybl.sld.layout.*;
 
+/**
+ * @author Kevin Le Saulnier <kevin.le-saulnier at rte-france.com>
+ */
 //TODO: to remove once SmartVoltageLevelLayoutFactory allow us to pass PositionVoltageLevelLayoutFactoryParameters to VoltageLevelLayoutFactory
 public interface CustomVoltageLevelLayoutFactoryCreator extends VoltageLevelLayoutFactoryCreator {
     static VoltageLevelLayoutFactoryCreator newCustomVoltageLevelLayoutFactoryCreator() {
