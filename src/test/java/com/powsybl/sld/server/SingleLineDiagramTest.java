94186fa046438eeef24da9ee40a09dc798cd373d
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2025-09-30 09:11:54 +0200

    fix: checkstyle
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/test/java/com/powsybl/sld/server/SingleLineDiagramTest.java
+++ src/test/java/com/powsybl/sld/server/SingleLineDiagramTest.java
@@ -52,7 +52,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
