3f9d3a22d5c46873beb76fdc9ba4f920658a6332
Caroline Jeandat <caroline.jeandat@rte-france.com>
2025-10-20 11:35:27 +0200

    remove "angle" from nad buses' legends
    Signed-off-by: Caroline Jeandat <caroline.jeandat@rte-france.com>

---
+++ src/main/java/com/powsybl/sld/server/NadLabelProvider.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.powsybl.sld.server;
+
+import com.powsybl.iidm.network.Bus;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.nad.model.BusNode;
+import com.powsybl.nad.svg.SvgParameters;
+import com.powsybl.nad.svg.iidm.DefaultLabelProvider;
+
+/**
+ * @author Caroline Jeandat <caroline.jeandat at rte-france.com>
+ */
+
+public class NadLabelProvider extends DefaultLabelProvider {
+    public NadLabelProvider(Network network, SvgParameters svgParameters) {
+        super(network, svgParameters);
+    }
+
+    @Override
+    public String getBusDescription(BusNode busNode) {
+        if (svgParameters.isBusLegend()) {
+            Bus b = network.getBusView().getBus(busNode.getEquipmentId());
+            String voltage = valueFormatter.formatVoltage(b.getV(), "kV");
+            return voltage;
+        }
+        return null;
+    }
+}
