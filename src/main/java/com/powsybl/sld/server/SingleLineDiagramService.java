614b87e650e12b4ffb36e78623d234e77ed5fbdf
Caroline Jeandat <caroline.jeandat@rte-france.com>
2025-10-27 16:14:24 +0100

    add parameter to display percentagePermanentLimit feederInfo
    Signed-off-by: Caroline Jeandat <caroline.jeandat@rte-france.com>

--- src/main/java/com/powsybl/sld/server/SingleLineDiagramService.java
+++ src/main/java/com/powsybl/sld/server/SingleLineDiagramService.java
@@ -92,6 +92,7 @@ class SingleLineDiagramService {
             svgParameters.setLanguageTag(diagParams.getLanguage());
             svgParameters.setUnifyVoltageLevelColors(true);
             svgParameters.setDisplayCurrentFeederInfo(true);
+            svgParameters.setDisplayPercentagePermanentLimitFeederInfo(true);
             LayoutParameters layoutParameters = new LayoutParameters(LAYOUT_PARAMETERS);
 
             SldParameters sldParameters = new SldParameters();
