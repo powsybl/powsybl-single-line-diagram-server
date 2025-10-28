6ed64b89de130e6f7f3d9aa59d7903de8970c7b9
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2025-10-28 16:56:34 +0100

    feat: add load and production sum to sld legend
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/main/java/com/powsybl/sld/server/CommonLabelProvider.java
+++ src/main/java/com/powsybl/sld/server/CommonLabelProvider.java
@@ -9,10 +9,26 @@ import com.powsybl.sld.svg.BusLegendInfo;
 import com.powsybl.sld.svg.DefaultLabelProvider;
 import com.powsybl.sld.svg.SvgParameters;
 
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.OptionalDouble;
+import java.util.stream.DoubleStream;
 
 public class CommonLabelProvider extends DefaultLabelProvider {
+    private static final String UNIT_MW = "MW";
+    private static final String UNIT_KV = "kV";
+
+    private static final String PREFIX_VOLTAGE = "U = ";
+    private static final String PREFIX_ANGLE = "θ = ";
+    private static final String PREFIX_GENERATOR_SUM = "P = ";
+    private static final String PREFIX_LOAD_SUM = "C = ";
+
+    private static final String KEY_BUS_ID = "busId";
+    private static final String KEY_VOLTAGE = "v";
+    private static final String KEY_ANGLE = "angle";
+    private static final String KEY_GENERATOR_SUM = "generatorSum";
+    private static final String KEY_LOAD_SUM = "loadSum";
+
     public CommonLabelProvider(Network network, SldComponentLibrary componentLibrary, LayoutParameters layoutParameters, SvgParameters svgParameters) {
         super(network, componentLibrary, layoutParameters, svgParameters);
     }
@@ -21,11 +37,29 @@ public class CommonLabelProvider extends DefaultLabelProvider {
     public List<BusLegendInfo> getBusLegendInfos(VoltageLevelGraph graph) {
         VoltageLevel vl = network.getVoltageLevel(graph.getVoltageLevelInfos().getId());
         return vl.getBusView().getBusStream()
-            .map(b -> new BusLegendInfo(b.getId(), List.of(
-                new BusLegendInfo.Caption(b.getId(), "busId"),
-                new BusLegendInfo.Caption(valueFormatter.formatVoltage(b.getV(), "kV"), "v"),
-                new BusLegendInfo.Caption(valueFormatter.formatAngleInDegrees(b.getAngle()), "angle")
-            )))
-            .collect(Collectors.toList());
+            .map(b ->
+                new BusLegendInfo(b.getId(), List.of(
+                    new BusLegendInfo.Caption(b.getId(), KEY_BUS_ID),
+                    new BusLegendInfo.Caption(PREFIX_VOLTAGE + valueFormatter.formatVoltage(b.getV(), UNIT_KV), KEY_VOLTAGE),
+                    new BusLegendInfo.Caption(PREFIX_ANGLE + valueFormatter.formatAngleInDegrees(b.getAngle()), KEY_ANGLE),
+                    new BusLegendInfo.Caption(PREFIX_GENERATOR_SUM + formatPowerSum(b.getGeneratorStream().mapToDouble(g -> g.getTerminal().getP())), KEY_GENERATOR_SUM),
+                    new BusLegendInfo.Caption(PREFIX_LOAD_SUM + formatPowerSum(b.getLoadStream().mapToDouble(l -> l.getTerminal().getP())), KEY_LOAD_SUM)
+                ))
+            ).toList();
+    }
+
+    private String formatPowerSum(DoubleStream stream) {
+        OptionalDouble sum = sumDoubleStream(stream);
+        String value = sum.isPresent()
+            ? String.valueOf(Math.round(Math.abs(sum.getAsDouble())))
+            : svgParameters.getUndefinedValueSymbol();
+        return value + " " + UNIT_MW;
+    }
+
+    private static OptionalDouble sumDoubleStream(DoubleStream stream) {
+        DoubleSummaryStatistics stats = stream.summaryStatistics();
+        return stats.getCount() == 0
+            ? OptionalDouble.empty()
+            : OptionalDouble.of(stats.getSum());
     }
 }
