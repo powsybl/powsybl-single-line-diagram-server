a001b4ab55fdb8a5046a80d1a9405c42643778da
Caroline Jeandat <caroline.jeandat@rte-france.com>
2025-11-05 16:36:15 +0100

    fix
    Signed-off-by: Caroline Jeandat <caroline.jeandat@rte-france.com>

--- src/main/java/com/powsybl/sld/server/dto/sld/SldRequestInfos.java
+++ src/main/java/com/powsybl/sld/server/dto/sld/SldRequestInfos.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.sld.server.dto.sld;
 
 import java.util.List;
@@ -10,6 +16,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * @author Caroline Jeandat <caroline.jeandat at rte-france.com>
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
