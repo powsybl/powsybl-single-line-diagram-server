5b0e226b4a3a184050feb9d6632beedcf0e7f7c6
Slimane AMAR <slimane.amar_externe@rte-france.com>
2025-12-03 11:41:30 +0100

    Fix merge
    Signed-off-by: Slimane AMAR <slimane.amar_externe@rte-france.com>

--- src/main/java/com/powsybl/sld/server/CommonLegendWriter.java
+++ src/main/java/com/powsybl/sld/server/CommonLegendWriter.java
@@ -9,32 +9,38 @@ package com.powsybl.sld.server;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.iidm.network.VoltageLevel;
 import com.powsybl.sld.model.graphs.VoltageLevelGraph;
-import com.powsybl.sld.svg.BusLegendInfo;
-import com.powsybl.sld.svg.DefaultSVGLegendWriter;
-import com.powsybl.sld.svg.SvgParameters;
+import com.powsybl.sld.svg.*;
 
-import java.util.DoubleSummaryStatistics;
-import java.util.List;
-import java.util.OptionalDouble;
+import java.util.*;
 import java.util.stream.DoubleStream;
 
 public class CommonLegendWriter extends DefaultSVGLegendWriter {
     private static final String UNIT_MW = "MW";
     private static final String UNIT_KV = "kV";
+    private static final String UNIT_KA = "kA";
 
     private static final String PREFIX_VOLTAGE = "U = ";
     private static final String PREFIX_ANGLE = "θ = ";
     private static final String PREFIX_PRODUCTION = "P = ";
     private static final String PREFIX_CONSUMPTION = "C = ";
+    private static final String PREFIX_ICC = "ICC = ";
 
     private static final String KEY_BUS_ID = "busId";
     public static final String KEY_VOLTAGE = "v";
     public static final String KEY_ANGLE = "angle";
     public static final String KEY_CONSUMPTION = "consumption-sum";
     public static final String KEY_PRODUCTION = "production-sum";
+    public static final String KEY_ICC = "icc";
 
-    public CommonLegendWriter(Network network, SvgParameters svgParameters) {
+    private final Map<String, Double> iccByBusId;
+
+    public static LegendWriterFactory createFactory(Map<String, Double> iccByBusId) {
+        return (network, svgParameters) -> new CommonLegendWriter(network, svgParameters, iccByBusId);
+    }
+
+    public CommonLegendWriter(Network network, SvgParameters svgParameters, Map<String, Double> iccByBusId) {
         super(network, svgParameters);
+        this.iccByBusId = iccByBusId;
     }
 
     @Override
@@ -47,9 +53,20 @@ public class CommonLegendWriter extends DefaultSVGLegendWriter {
                     new BusLegendInfo.Caption(PREFIX_VOLTAGE + valueFormatter.formatVoltage(b.getV(), UNIT_KV), KEY_VOLTAGE),
                     new BusLegendInfo.Caption(PREFIX_ANGLE + valueFormatter.formatAngleInDegrees(b.getAngle()), KEY_ANGLE),
                     new BusLegendInfo.Caption(PREFIX_PRODUCTION + formatPowerSum(b.getGeneratorStream().mapToDouble(g -> g.getTerminal().getP())), KEY_CONSUMPTION),
-                    new BusLegendInfo.Caption(PREFIX_CONSUMPTION + formatPowerSum(b.getLoadStream().mapToDouble(l -> l.getTerminal().getP())), KEY_PRODUCTION)
+                    new BusLegendInfo.Caption(PREFIX_CONSUMPTION + formatPowerSum(b.getLoadStream().mapToDouble(l -> l.getTerminal().getP())), KEY_PRODUCTION),
+                    new BusLegendInfo.Caption(PREFIX_ICC + getFormattedBusIcc(b.getId()), KEY_ICC)
                 ))
             ).toList();
+    }
+
+    private String getFormattedBusIcc(String busId) {
+        Double iccInA = iccByBusId == null ? null : iccByBusId.get(busId);
+
+        String value = iccInA != null
+            ? String.format(Locale.US, "%.1f", iccInA / 1000.0)
+            : svgParameters.getUndefinedValueSymbol();
+
+        return value + " " + UNIT_KA;
     }
 
     private String formatPowerSum(DoubleStream stream) {
