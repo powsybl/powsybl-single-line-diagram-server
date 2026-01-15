73242143415bcc3b4b7b24396dc7daaaac74c96c
Hugo Marcellin <hugo.marcelin@rte-france.com>
2026-01-15 11:52:02 +0100

    Increase coverage Signed-off-by: Hugo Marcellin <hugo.marcelin@rte-france.com>

---
+++ src/test/java/com/powsybl/sld/server/error/SingleLineDiagramBusinessErrorCodeTest.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.sld.server.error;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+ * @author Hugo Marcellin <hugo.marcelin at rte-france.com>
+ */
+class SingleLineDiagramBusinessErrorCodeTest {
+    @ParameterizedTest
+    @EnumSource(SingleLineDiagramBusinessErrorCode.class)
+    void valueMatchesEnumName(SingleLineDiagramBusinessErrorCode code) {
+        assertThat(code.value()).startsWith("singleLineDiagram..");
+    }
+}
