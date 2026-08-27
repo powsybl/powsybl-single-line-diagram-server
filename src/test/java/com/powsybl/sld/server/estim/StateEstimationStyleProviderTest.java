6bb0c7431adf8058b7d3c8f5843bdebf2c41b6f1
Kamil MARUT <kamil.marut@rte-france.com>
2026-08-27 16:58:00 +0200

    refactor(sld-visualisation): determine estim styles through dedicated feeder info instead of userDefinedId
    Signed-off-by: Kamil MARUT <kamil.marut@rte-france.com>

--- src/test/java/com/powsybl/sld/server/estim/StateEstimationStyleProviderTest.java
+++ src/test/java/com/powsybl/sld/server/estim/StateEstimationStyleProviderTest.java
@@ -11,11 +11,16 @@ package com.powsybl.sld.server.estim;
 import com.powsybl.sld.svg.FeederInfo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mockito;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Kamil MARUT {@literal <kamil.marut at rte-france.com>}
@@ -24,28 +29,35 @@ class StateEstimationStyleProviderTest {
 
     private StateEstimationStyleProvider provider;
 
+    private static Stream<Arguments> provideEstimMeasurementsInfos() {
+        return Stream.of(
+                Arguments.of(true, true, List.of("sld-measurement-valid", "sld-measurement-critical")),
+                Arguments.of(false, false, List.of("sld-measurement-invalid")),
+                Arguments.of(true, false, List.of("sld-measurement-valid")),
+                Arguments.of(false, true, List.of("sld-measurement-invalid", "sld-measurement-critical"))
+        );
+    }
+
     @BeforeEach
     void setUp() {
         provider = new StateEstimationStyleProvider();
     }
 
-    @Test
-    void testGetFeederInfoStylesReturnsListOfUserDefinedIdWhenNotNull() {
-        String userDefinedId = "hello world";
-        FeederInfo feederInfoMock = Mockito.mock(FeederInfo.class);
-        Mockito.when(feederInfoMock.getUserDefinedId()).thenReturn(userDefinedId);
+    @ParameterizedTest
+    @MethodSource("provideEstimMeasurementsInfos")
+    void testGetFeederInfoStylesReturnsExpectedStyles(boolean isValid, boolean isCritical, List<String> expectedStyles) {
+        EstimMeasurementsFeederInfo feederInfoMock = Mockito.mock(EstimMeasurementsFeederInfo.class);
+        when(feederInfoMock.isValid()).thenReturn(isValid);
+        when(feederInfoMock.isCritical()).thenReturn(isCritical);
 
         List<String> actualList = provider.getFeederInfoStyles(feederInfoMock);
 
-        assertThat(actualList)
-                .containsExactly(userDefinedId)
-                .size().isEqualTo(1);
+        assertThat(actualList).containsExactlyElementsOf(expectedStyles);
     }
 
     @Test
-    void testGetFeederInfoStylesReturnsEmptyListWhenUserDefinedIdNull() {
+    void testGetFeederInfoStylesReturnsEmptyListWhenNotEstimFeederInfos() {
         FeederInfo feederInfoMock = Mockito.mock(FeederInfo.class);
-        Mockito.when(feederInfoMock.getUserDefinedId()).thenReturn(null);
 
         List<String> actualList = provider.getFeederInfoStyles(feederInfoMock);
 
